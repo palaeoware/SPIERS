d087f6df920c3a47cee88fcf83293eca4fdeb034
Alan Spencer <a.r.t.spencer@gmail.com>
2018-10-16 19:11:51 +0100

    Updated file comments

--- netmodule.h
+++ netmodule.h
@@ -1,3 +1,20 @@
+/**
+ * @file
+ * Header: NetModule
+ *
+ * All SPIERScommon code is released under the GNU General Public License.
+ * See LICENSE.md files in the programme directory.
+ *
+ * All SPIERSview code is Copyright 2008-2018 by Russell J. Garwood, Mark D. Sutton,
+ * and Alan R.T. Spencer.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY.
+ */
+
 #ifndef NETMODULE_H
 #define NETMODULE_H
 
