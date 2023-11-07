46503cba0d30c313c96c8c6d90c910de7cfcd38d
Mark Sutton <m.sutton@imperial.ac.uk>
2023-11-07 09:52:26 +0000

    Removed some traceprints and extraneous stuff

--- SPIERSedit/src/beamhardening.h
+++ SPIERSedit/src/beamhardening.h
@@ -10,15 +10,11 @@ class BeamHardening
 {
 public:
     BeamHardening();
-    void FindCentre(QList<int> *slices, int maxRadius);
     void Measure(QListWidget *SliceSelectorList, int cX, int cY, QLabel *label, int radius);
     int GetCorrectionAtWorkingImageCoordinates(int x, int y);
     bool HasSample();
     void SetParams(int x, int y, int radius, int adjust);
 private:
-    void CombineIntoData(int slice, int *accumulator);
-    double ScorePixel(int, int, int, int *, int *buffer);
-    int SampleAccumulator(double, double, int *accumulator);
     int *sampleBuffer, *sampleCountBuffer;
     int sampleBufferSize;
     long Sample(int file, int centerX, int centerY);
