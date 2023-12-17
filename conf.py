fde075d623113bdedfb38678585836bb4ff97596
RussellGarwood <russell.garwood@manchester.ac.uk>
2023-12-17 13:46:08 +0000

    Improve comments

--- conf.py
+++ conf.py
@@ -9,7 +9,7 @@ extensions = [
 ]
 
 # Define the projects that will share this configuration file.
-# These have to match the project environmental variable defined in the readthedocs admin menu (readthedocs project --> admin --> environmental variables)
+# These have to match the project environmental variable defined in the readthedocs admin menu (readthedocs project --> admin --> environmental variables; i.e.SPIERSalign/docs,SPIERSedit/docs or SPIERSview/docs)
 multiproject_projects = {
     "SPIERSalign/docs": {
          "config": {
