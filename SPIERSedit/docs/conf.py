f373dac69c50d59cc8530922e4596ad13d9caf45
RussellGarwood <russell.garwood@manchester.ac.uk>
2023-12-27 21:38:22 +0000

    updated view and edit so logo works in docs

--- SPIERSedit/docs/conf.py
+++ SPIERSedit/docs/conf.py
@@ -104,7 +104,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['SPIERSedit/docs/_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -125,7 +125,7 @@ html_static_path = ['_static']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/SPIERS.jpg'
+html_logo = 'SPIERSedit/docs/_static/SPIERS.jpg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
